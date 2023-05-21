f2172addd26c0de69bf0650b72a0a17a6b17a2aa
emichal04 <132500335+emichal04@users.noreply.github.com>
2023-05-21 18:59:26 +0200

    Publikacja nowej wersji strony.
    - Zmiana układu graficznego. 
    - Usunięcie z arkusza CSS nieużywanych klas.
    - Modyfikacja witryny dla większej kompatybilności z przeglądarkami tekstowymi.
    - Podział strony głównej na sekcje.
    - Dodanie favicony.

---
+++ skrypt1.js
@@ -0,0 +1,4 @@
+
+function menu() {
+    document.body.innerHTML += "<div class='menu'><div class='w3-bar adfs'><a href='index.html' class='w3-bar-item w3-button w3-mobile w3-hover-light-blue w3-padding' style='width:25%'>/home</a><a href='posty.html' class='w3-bar-item w3-button w3-mobile w3-hover-light-blue w3-padding' style='width:25%'>Posty</a><a href='filmy.html' class='w3-bar-item w3-button w3-mobile w3-hover-light-blue w3-padding' style='width:25%'>Filmy</a><a href='sprzet.html' class='w3-bar-item w3-button w3-mobile w3-hover-light-blue w3-padding' style='width:25%'>Sprzęt</a></div></div>";
+}
